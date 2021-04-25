c7d836928f0b04a18e79828fbc2094c427817f53
Shinya <cgyceusc@icloud.com>
2021-04-25 07:12:13 +0000

    Rails 04 User新規登録に潜む問題を解決していく

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  #name 空白NG １５文字以内
+  validates :name, presence: true, length: {maximum: 15}
+  #「~ @ ~ . ~」の形式のみ許可
+  valid_email_regex = /\w+@\w+.\w+/
+  validates :email, presence: true, format: {with: valid_email_regex}
+  #8~32文字　アルファベット・数字の混合のみ許可
+  valid_password_regex = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, length: {in: 8..32}, format: {with: valid_password_regex} 
+  #8~32文字　アルファベット・数字の混合のみ許可
+  validates :password_confirmation, length: {in: 8..32}, format: {with: valid_password_regex} 
+  
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+  
+  resource :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
