2f5902681abaa59a7770ceee29094aadfea1b4dc
Shinya <cgyceusc@icloud.com>
2021-04-26 13:34:45 +0000

    Rails 05 ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
+  # get 'users/new'
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
   
-  resource :users
+  resources :users
+  
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destory'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
